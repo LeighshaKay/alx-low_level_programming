14c4d2fb062fb6688ae6290bab1be7d1901c184a
LeighshaKay <ashleigh.kusowa@gmail.com>
2022-04-21 02:49:21 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,18 +1,15 @@
 #include "main.h"
 
-
-
 /**
+ *reset_to_98 - Updates the value it points to 98
  *
- *   * reset_to_98 - Updates the value it points to 98
+ *@n: pointer to n
  *
- *     * @n: pointer to n
  *
- *       *
  *
- *         * Return: void
+ *Return: void
  *
- *           */
+ */
 
 void reset_to_98(int *n)
 
