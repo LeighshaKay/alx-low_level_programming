0698100207733bb4d7b94b4a78857253b57c54a8
LeighshaKay <ashleigh.kusowa@gmail.com>
2022-04-25 06:24:58 -0700

    Task 2 3

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,47 +1,21 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- *simple_print_buffer - prints buffer in hexa
- *@buffer: the address of memory to print
- *@size: the size of the memory to print
+ *_memset - Fills the first n bytes of the memory area
+ *         pointed to by @s with the constant byte @c.
+ *@s: A pointer to the memory area to be filled.
+ *@c: The character to fill the memory area with.
+ *@n: The number of bytes to be filled.
  *
- *Return: Nothing.
- *
+ *Return: A pointer to the filled memory area @s.
  */
-void simple_print_buffer(char *buffer, unsigned int size)
+void *_memset(void *s, int c, size_t n)
 {
-	unsigned int i;
+	unsigned int index;
+	unsigned char *memory = s, value = c;
 
-	i = 0;
-	while (i < size)
-	{
-		if (i % 10)
-		{
-			printf(" ");
-		}
-		if (!(i % 10) && i)
-		{
-			printf("\n");
-		}
-		printf("0x%02x", buffer[i]);
-		i++;
-	}
-	printf("\n");
-}
+	for (index = 0; index < n; index++)
+		memory[index] = value;
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	char buffer[98] = {0x00};
-
-	simple_print_buffer(buffer, 98);
-	_memset(buffer, 0x01, 95);
-	printf("-------------------------------------------------\n");
-	simple_print_buffer(buffer, 98);
-	return (0);
+	return (memory);
 }
